1e5741ea84d7bad0b6471ff6633369e53bdfd5a1
eng-asano <eng.y.asano@gmail.com>
2024-11-30 21:47:20 +0900

    feat: ユーザーのサムネイルを取得

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image'
+import { ThumbnailResponse } from '@/src/types/api'
 import { css } from '@/styled-system/css'
 import { flex } from '@/styled-system/patterns'
 
-export const Profile = () => {
+export const Profile = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/thumbnails/us-tech/test1`)
+  const { img } = (await res.json()) as ThumbnailResponse
+
   return (
     <section className={styles.root}>
-      <Image src="/images/man.png" width={120} height={120} alt="profile thumbnail" className={styles.thumbnail} />
+      <Image src={img} width={120} height={120} alt="profile thumbnail" className={styles.thumbnail} />
       <h2 className={styles.name}>David Peters</h2>
       <span className={styles.post}>Senior Developer</span>
     </section>
---
+++ src/types/api.ts
@@ -0,0 +1,4 @@
+export type ThumbnailResponse = {
+  img: string
+  error?: string
+}

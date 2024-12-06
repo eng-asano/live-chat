721728f2ac8e185f7b05aa6243654eb62cd1001e
eng-asano <eng.y.asano@gmail.com>
2024-12-06 20:20:04 +0900

    refactor: types

--- src/types/cognito.ts
+++ src/types/cognito.ts
@@ -1,10 +1,10 @@
 export interface UserInfo {
-  'custom:team_code': string
   'cognito:username': string
+  'custom:team_code': string
   name: string
   given_name: string
   family_name: string
   'custom:post': string
   'custom:thumbnail_key': string
-  'custom:is_active': 0 | 1
+  sub: string
 }

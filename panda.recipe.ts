af7d7f31b44efb20e71bc62e3937b9e8670ee6f5
eng-asano <eng.y.asano@gmail.com>
2024-12-09 17:59:57 +0900

    style: 不要なコメント削除

--- panda.recipe.ts
+++ panda.recipe.ts
@@ -46,47 +46,6 @@ export const messageTextRecipe = defineRecipe({
     lineHeight: '1.8rem',
     bgColor: 'message.member',
     borderRadius: '48px',
+    whiteSpace: 'pre-wrap',
   },
 })
-
-// export const neumorphismDentRecipe = defineRecipe({
-//   className: 'neumorphism.dent',
-//   description: 'The neumorphism styles',
-//   base: {
-//     border: '1px solid #c9c9c9',
-//     outline: 'none',
-//   },
-//   variants: {
-//     type: {
-//       input: {
-//         // boxShadow: 'inset 3px 3px 6px #babecc, inset -3px -3px 6px #fff',
-//       },
-//       avatar: {
-//         boxShadow: 'inset 3px 3px 6px #babecc, inset -3px -3px 6px #fff',
-//       },
-//     },
-//   },
-// })
-
-// export const neumorphismDumpRecipe = defineRecipe({
-//   className: 'neumorphism.dump',
-//   description: 'The neumorphism styles',
-//   base: {
-//     border: '1px solid #e9e9e9',
-//     outline: 'none',
-//   },
-//   variants: {
-//     type: {
-//       button: {
-//         cursor: 'pointer',
-//         // boxShadow: '3px 3px 6px #babecc, -3px -3px 6px #fff',
-//         _active: {
-//           // boxShadow: 'inset 3px 3px 6px #babecc, inset -3px -3px 6px #fff',
-//           _disabled: {
-//             // boxShadow: '3px 3px 6px #babecc, -3px -3px 6px #fff',
-//           },
-//         },
-//       },
-//     },
-//   },
-// })

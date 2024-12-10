0da039e866b382f18a350a560fa4ec0aeab72ab6
eng-asano <eng.y.asano@gmail.com>
2024-12-10 23:33:12 +0900

    refactor: メッセージ取得

--- src/hooks/useLiveChat.ts
+++ src/hooks/useLiveChat.ts
@@ -21,7 +21,9 @@ type Data =
 interface SocketData {
   activeUserIds: string[]
   messages: Message[]
+  hasMoreMessage: boolean
   sendContent: (content: string, type: string) => void
+  loadPrevMessages: () => Promise<void>
 }
 
 export const useLiveChat = (teamCode: string, userId: string) => {
@@ -33,7 +35,7 @@ export const useLiveChat = (teamCode: string, userId: string) => {
     client.addEventListener('open', async () => {
       const activeUserIds = await fetchActiveUsers(teamCode)
 
-      const messages = await fetchMessages(teamCode)
+      const { messages, hasMoreMessage } = await fetchMessages(teamCode)
 
       // メッセージを送信する関数
       const sendContent = (content: string, type: string) => {
@@ -47,29 +49,62 @@ export const useLiveChat = (teamCode: string, userId: string) => {
         client.send(JSON.stringify(body))
       }
 
-      next(undefined, { activeUserIds, messages, sendContent })
+      // 過去のメッセージを読み込む
+      const loadPrevMessages = async () => {
+        next(undefined, async (prev) => {
+          const createdAt = prev?.messages[0]?.created_at
+          if (!createdAt) return prev
+
+          const { messages, hasMoreMessage } = await fetchMessages(teamCode, createdAt)
+
+          return {
+            ...prev,
+            hasMoreMessage,
+            messages: [...messages, ...prev.messages],
+          }
+        })
+      }
+
+      // 接続確立時の初期値
+      next(undefined, {
+        activeUserIds,
+        messages: messages.reverse(), // 初期表示は昇順で表示する
+        hasMoreMessage,
+        sendContent,
+        loadPrevMessages,
+      })
     })
 
     client.addEventListener('message', (event) => {
       const d = JSON.parse(event.data) as Data
 
-      if (d.action === 'connect' || d.action === 'disconnect') {
-        next(undefined, (prev) => {
-          return prev && { ...prev, activeUserIds: d.data.activeUserIds }
-        })
-      }
+      next(undefined, (prev) => {
+        if (!prev) return prev
 
-      if (d.action === 'message') {
-        next(undefined, (prev) => {
-          return prev && { ...prev, activeUserIds: d.data.activeUserIds, messages: [...prev.messages, d.data.messages] }
-        })
-      }
+        switch (d.action) {
+          case 'connect':
+          case 'disconnect':
+            // ログイン中のユーザーIDを取得
+            return {
+              ...prev,
+              activeUserIds: d.data.activeUserIds,
+            }
+          case 'message':
+            // ログイン中のユーザーIDと他のユーザーからのメッセージを取得
+            return {
+              ...prev,
+              activeUserIds: d.data.activeUserIds,
+              messages: [...prev.messages, d.data.messages],
+            }
+          default:
+            return prev
+        }
+      })
     })
 
-    client.addEventListener('error', () => {})
-
     return () => {
-      if (client.readyState !== client.CLOSING) {
+      // 接続が確立していない状態でのcloseを防止
+      if (client?.readyState !== client?.CLOSING) {
         client?.close()
       }
     }
@@ -78,7 +113,9 @@ export const useLiveChat = (teamCode: string, userId: string) => {
   return {
     activeUserIds: data?.activeUserIds ?? [],
     messages: data?.messages ?? [],
+    hasMoreMessage: data?.hasMoreMessage || false,
     sendContent: data?.sendContent,
+    loadPrevMessages: data?.loadPrevMessages,
   }
 }
 
@@ -86,13 +123,18 @@ export const useLiveChat = (teamCode: string, userId: string) => {
 async function fetchActiveUsers(teamCode: string) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_ROUTE_URL}/api/teams/${teamCode}/active-users`)
   const data = (await res.json()) as Session[]
-  const ids = data.map(({ user_id }) => user_id)
-  return ids
+
+  return data.map(({ user_id }) => user_id)
 }
 
 /** 同じteam_codeのメッセージ履歴を取得 */
-async function fetchMessages(teamCode: string) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_ROUTE_URL}/api/teams/${teamCode}/messages`)
-  const data = (await res.json()) as Message[]
-  return data
+async function fetchMessages(teamCode: string, createdAt?: string) {
+  // 指定した作成日以前のメッセージを取得するためクエリパラメーター
+  // 空値の場合は直近のメッセージを取得
+  const query = createdAt ? `?lastMessageAt=${createdAt}` : ''
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_ROUTE_URL}/api/${teamCode}/messages${query}`)
+  const data = await res.json()
+
+  return data as { messages: Message[]; hasMoreMessage: boolean }
 }

022c3639354ff2a411b9d47c1ed65e5652c7a383
eng-asano <eng.y.asano@gmail.com>
2024-12-28 00:37:42 +0900

    refactor: data format

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -3,16 +3,16 @@ import 'moment/locale/ja'
 
 /** ISO8601の日付、時刻を変換 */
 export function formatISO8601(isoDate: string) {
-  moment.locale('ja')
+  moment.locale('en')
 
   const today = moment().startOf('day')
   const targetDate = moment(isoDate).startOf('day')
 
   if (targetDate.isSame(today, 'day')) {
-    return '今日'
+    return 'Today'
   } else if (targetDate.isSame(today.clone().subtract(1, 'day'), 'day')) {
-    return '昨日'
+    return 'Yesterday'
   } else {
-    return targetDate.format('M月D日(ddd)')
+    return targetDate.format('M/D')
   }
 }
